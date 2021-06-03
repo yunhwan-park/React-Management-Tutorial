8db00a37347c155064d046da95a2c82bb8e1d58a
yunhwan-park <sevenwind@weclover.co.kr>
2021-06-03 17:54:42 +0900

    클라이언트 폴더로 프론트 엔드 몰아 넣음
    프론트 엔드 폴더

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import './App.css';
+import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+const customers = [
+  {
+    'id':1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name':'홍길이',
+    'birthday':'961222',
+    'gender':'남자',
+    'job':'대학생',
+  },
+  {
+    'id':2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name':'숫돌이',
+    'birthday':'200101',
+    'gender':'남자',
+    'job':'고딩',
+  },
+  {
+    'id':3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name':'나대주',
+    'birthday':'900423',
+    'gender':'여자',
+    'job':'직장인',
+  },
+]
+class App extends Component
+{
+  render() {
+
+    const { classes } = this.props;
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+          <TableBody>
+          {
+            customers.map(c =>{
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              )
+            })
+          }
+          </TableBody>
+        </Table>
+      </Paper>
+    )
+  };
+}
+
+export default withStyles(styles)(App);
