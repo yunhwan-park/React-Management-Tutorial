1482ae8fe6685a3a381671cbbf7d366e5dafe927
yunhwan-park <sevenwind@weclover.co.kr>
2021-06-04 14:42:01 +0900

    DB 적용함
    Nodejs DB connect 설정

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');;
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connecttion = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connecttion.connect()
+
 app.get('/api/customers', (req, res) => {
-    res.send(
-        [
-            {
-                'id':1,
-                'image': 'https://placeimg.com/64/64/1',
-                'name':'홍길이',
-                'birthday':'961222',
-                'gender':'남자',
-                'job':'대학생',
-              },
-              {
-                'id':2,
-                'image': 'https://placeimg.com/64/64/2',
-                'name':'숫돌이',
-                'birthday':'200101',
-                'gender':'남자',
-                'job':'고딩',
-              },
-              {
-                'id':3,
-                'image': 'https://placeimg.com/64/64/3',
-                'name':'나대주',
-                'birthday':'900423',
-                'gender':'여자',
-                'job':'직장인',
-              },
-        ]
+    connecttion.query(
+      "select * from customer",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
     );
 });
 
