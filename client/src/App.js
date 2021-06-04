23af3ecb567055be1bc5d55582fbb07f0a30cb22
yunhwan-park <sevenwind@weclover.co.kr>
2021-06-04 18:02:43 +0900

    15강 고객 데이터 삭제 및 칼럼 추가 작업
    수정

--- client/src/App.js
+++ client/src/App.js
@@ -98,6 +98,7 @@ class App extends Component
             <TableCell>생년월일</TableCell>
             <TableCell>성별</TableCell>
             <TableCell>직업</TableCell>
+            <TableCell>설정</TableCell>
           </TableRow>
         </TableHead>
           <TableBody>
@@ -105,6 +106,7 @@ class App extends Component
               this.state.customers ? this.state.customers.map(c =>{
                 return (
                   <Customer
+                    stateRefresh={this.stateRefresh}
                     key={c.id}
                     id={c.id}
                     image={c.image}
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const upload = multer({dest : './upload'});
 
 app.get('/api/customers', (req, res) => {
     connecttion.query(
-      "select * from customer",
+      "select * from customer where isDeleted = 0",
       (err, rows, fields) => {
         res.send(rows);
       }
@@ -36,7 +36,7 @@ app.use('/image', express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
   
-  let sql = 'insert into customer values (null, ?, ?, ?, ?, ?)';
+  let sql = 'insert into customer values (null, ?, ?, ?, ?, ?, now(), 0)';
   
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
@@ -53,5 +53,16 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
     }
   );
 }); 
+
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = "update customer set isDeleted = 1 where id = ?";
+  let params = [req.params.id];
+
+  connecttion.query(sql, params, 
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
+});
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
