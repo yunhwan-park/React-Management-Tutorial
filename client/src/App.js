94e8e0ff1070411900f6b15b8016eb8378a529cc
yunhwan-park <sevenwind@weclover.co.kr>
2021-06-04 10:13:19 +0900

    8강까지 완료한 내용 반영
    8강에서는 서버 네트워드 설정 및 프록시 설정까지 처음으로 서버와 클라이언트를 연결하는 작업을 진행했음

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,26 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id':1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name':'홍길이',
-    'birthday':'961222',
-    'gender':'남자',
-    'job':'대학생',
-  },
-  {
-    'id':2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name':'숫돌이',
-    'birthday':'200101',
-    'gender':'남자',
-    'job':'고딩',
-  },
-  {
-    'id':3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name':'나대주',
-    'birthday':'900423',
-    'gender':'여자',
-    'job':'직장인',
-  },
-]
 class App extends Component
 {
+  state = {
+    customers: ""
+  }
+
+  componentDidMount()
+  {
+    this.callApi()
+    .then(res => this.setState({customers:res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+
+    return body;
+  }
+
   render() {
 
     const { classes } = this.props;
@@ -67,19 +59,21 @@ class App extends Component
         </TableHead>
           <TableBody>
           {
-            customers.map(c =>{
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              )
-            })
+              this.state.customers ? this.state.customers.map(c =>{
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                )
+              })
+              :
+              ""
           }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send(
+        [
+            {
+                'id':1,
+                'image': 'https://placeimg.com/64/64/1',
+                'name':'홍길이',
+                'birthday':'961222',
+                'gender':'남자',
+                'job':'대학생',
+              },
+              {
+                'id':2,
+                'image': 'https://placeimg.com/64/64/2',
+                'name':'숫돌이',
+                'birthday':'200101',
+                'gender':'남자',
+                'job':'고딩',
+              },
+              {
+                'id':3,
+                'image': 'https://placeimg.com/64/64/3',
+                'name':'나대주',
+                'birthday':'900423',
+                'gender':'여자',
+                'job':'직장인',
+              },
+        ]
+    );
 });
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
