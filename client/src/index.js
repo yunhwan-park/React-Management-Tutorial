3d5de0292232ed7724cdb29aa81b3f1469d989c2
yunhwan-park <sevenwind@weclover.co.kr>
2021-06-07 11:46:04 +0900

    Update UI/UX & add Customer Search Module
    18강까지의 모든 내용을 업로드 합니다.

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Not Sans KR, seif',
+  }
+})
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <MuiThemeProvider theme={theme}>
+  <App />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
